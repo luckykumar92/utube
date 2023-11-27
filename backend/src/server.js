4ac29333cd47b202c6e576444ecf4b5f83cbcf98
Lucky Kumar <lucky.kumar@lpu.in>
2023-11-27 17:45:37 +0530

    util code discussion

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,11 +3,21 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
+const port = process.env.PORT || 8000;
+
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is running at port: ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection failed!!!", err);
+  });
 
 /*
 import express from "express";
