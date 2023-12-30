2a9253653f00758b368b3dd8c82f6bb59c40c006
Lucky Kumar <lucky.kumar@lpu.in>
2023-12-30 12:58:54 +0530

    monor changes in usercontroller

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -271,7 +271,7 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
 const getCurrentUser = asyncHandler(async (req, res) => {
   return res
     .status(200)
-    .json(200, req.user, "current user fetched successfully");
+    .json(new ApiResponse(200, req.user, "current user fetched successfully"));
 });
 
 // ################### Update Account Detail #######################
@@ -283,7 +283,7 @@ const updateAccountDetails = asyncHandler(async (req, res) => {
     throw new ApiError(400, "All fields are required");
   }
 
-  const user = User.findByIdAndUpdate(
+  const user = await User.findByIdAndUpdate(
     req.user?._id,
     {
       $set: {
@@ -306,6 +306,8 @@ const updateUserAvatar = asyncHandler(async (req, res) => {
   if (avatarLocalPath) {
     throw new ApiError(400, "Avatar file is missing");
   }
+
+  //TODO: delete old image
 
   const avatar = await uploadOnCloudinary(avatarLocalPath);
 
